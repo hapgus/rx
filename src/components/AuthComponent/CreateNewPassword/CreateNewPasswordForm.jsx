e627c643cbe864a8e56ae99fcea68341b8746b03
andrejohnson-dev <aj.sfw.developer@gmail.com>
2024-10-02 12:54:50 -0400

    reset.

--- src/components/AuthComponent/CreateNewPassword/CreateNewPasswordForm.jsx
+++ src/components/AuthComponent/CreateNewPassword/CreateNewPasswordForm.jsx
@@ -104,7 +104,7 @@ export const CreateNewPasswordForm = () => {
                 const { responseData, responseStatusCode } = await sendRequest(
 
                     `${process.env.REACT_APP_BACKEND_URL}reset/${token}`,
-                  
+                    // `http://localhost:3005/new-password/`,
                     'POST',
                     JSON.stringify(data), {
                     'Content-Type': 'application/json',
